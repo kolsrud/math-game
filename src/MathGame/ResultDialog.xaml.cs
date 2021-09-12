f8a64e58c8641fe181e76a8651e33c3a18547ac7
Øystein Kolsrud <yko@qlik.com>
2021-09-12 20:41:06 +0200

    Lots of changes.

---
+++ src/MathGame/ResultDialog.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathGame
+{
+	/// <summary>
+	/// Interaction logic for ResultDialog.xaml
+	/// </summary>
+	public partial class ResultDialog
+    {
+        private TaskCompletionSource<bool> _tc;
+
+		public ResultDialog(ViewModel viewModel, TaskCompletionSource<bool> tc)
+		{
+			InitializeComponent();
+            DataContext = viewModel;
+            _tc = tc;
+        }
+
+        private void ButtonExit_OnClick(object sender, RoutedEventArgs e)
+        {
+            _tc.SetResult(false);
+        }
+
+        private void ButtonPlayAgain_OnClick(object sender, RoutedEventArgs e)
+        {
+            _tc.SetResult(true);
+        }
+    }
+}
--- src/MathGame/ViewModel.cs
+++ src/MathGame/ViewModel.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 
 namespace MathGame
 {
+    public class Player
+    {
+        public string Name { get; set; }
+        public TimeSpan BestTime { get; set; }
+    }
+
+    public class ProblemResult
+    {
+        public string Status { get; set; }
+        public string Problem { get; set; }
+    }
+
     public class ViewModel
     {
         public ObservableValue<string> Problem { get; } = new ObservableValue<string>();
         public ObservableValue<string> Solution { get; } = new ObservableValue<string>();
+        public ObservableValue<int> ProblemCnt { get; } = new ObservableValue<int>();
+        public ObservableValue<int> ProblemCntCorrect { get; } = new ObservableValue<int>();
+        public ObservableValue<string> ProblemCntCorrectPercentage { get; } = new ObservableValue<string>("(0%)");
+        public ObservableValue<int> ProblemCntWrong { get; } = new ObservableValue<int>();
+        public ObservableValue<string> ProblemCntWrongPercentage { get; } = new ObservableValue<string>("(0%)");
+        public ObservableValue<string> Time { get; } = new ObservableValue<string>();
+
+        private Dictionary<int, int> _failCounts = new Dictionary<int, int>();
+
+        private readonly Stopwatch _sw = new Stopwatch();
+        private readonly Timer _timer = new Timer(100);
+
+        public ObservableCollection<ProblemResult> ProblemHistory1 { get; } = new ObservableCollection<ProblemResult>();
+        public ObservableCollection<ProblemResult> ProblemHistory2 { get; } = new ObservableCollection<ProblemResult>();
 
         private int _x;
         private int _y;
@@ -14,7 +46,41 @@ namespace MathGame
 
         public ViewModel()
         {
+            NewGame();
+            _timer.AutoReset = true;
+            _timer.Elapsed += TimerOnElapsed;
+            _timer.Start();
             NewProblem();
+        }
+
+        private void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            Time.Value = ElapsedTimeString;
+        }
+
+        private TimeSpan ComputeTime()
+        {
+            return _sw.Elapsed + AddedTime;
+        }
+
+        private TimeSpan AddedTime => TimeSpan.FromSeconds(ProblemCntWrong.Value * 30);
+
+        private string ElapsedTimeString => ComputeTime().ToString(@"m\:ss");
+
+        private void NewGame()
+        {
+            ProblemCnt.Value = 0;
+            ProblemCntCorrect.Value = 0;
+            ProblemCntWrong.Value = 0;
+            ProblemCntCorrectPercentage.Value = "(0%)";
+            ProblemCntWrongPercentage.Value = "(0%)";
+            _sw.Reset();
+            ProblemHistory1.Clear();
+            ProblemHistory2.Clear();
+            foreach (var i in Enumerable.Range(0, 11))
+            {
+                _failCounts[i] = 0;
+            }
         }
 
         private void NewProblem()
@@ -25,17 +91,76 @@ namespace MathGame
             Problem.Value = _x + " * " + _y;
         }
 
-        public void CheckAnswer()
+        public string ResultSummary =>
+            $"Din tid blev {ElapsedTimeString} ({_sw.Elapsed:m\\:ss} + {AddedTime:m\\:ss}) och du fick {100 * ProblemCntCorrect.Value / ProblemCnt.Value:D}% rätt!";
+
+        public string DetailedResult => GetDetailedResults();
+
+        private string GetDetailedResults()
+        {
+            return string.Join(Environment.NewLine, _failCounts.OrderBy(kv => kv.Key).Select(GetDetailedResult));
+        }
+
+        private string GetDetailedResult(KeyValuePair<int, int> kv)
+        {
+            return $"{kv.Key}: {kv.Value} fel";
+        }
+
+        public async void CheckAnswer()
         {
             if (string.IsNullOrEmpty(Solution.Value))
                 return;
 
+            _sw.Start();
+            ProblemCnt.Value += 1;
+            var problemResult = new ProblemResult();
+            problemResult.Problem = Problem.Value;
             if (int.Parse(Solution.Value) == _x * _y)
-                MessageBox.Show("Grattis! Det var rätt!");
+            {
+                ProblemCntCorrect.Value += 1;
+                problemResult.Status = "R";
+            }
             else
-                MessageBox.Show("Ajdå! Det blev fel... Rätt svar är " + _x*_y);
+            {
+                ProblemCntWrong.Value += 1;
+                problemResult.Status = "V";
+                MessageBox.Show("Ajdå! Det blev fel... Du förlorar 30 sekunder! Rätt svar är " + _x * _y, "Fel svar!", MessageBoxButton.OK, MessageBoxImage.Error);
+                _failCounts[_x]++;
+                if (_y != _x)
+                    _failCounts[_y]++;
+            }
+
+            ProblemCntCorrectPercentage.Value = $"({100 * ProblemCntCorrect.Value / ProblemCnt.Value:D}%)";
+            ProblemCntWrongPercentage.Value = $"({100 * ProblemCntWrong.Value / ProblemCnt.Value:D}%)";
+
+            if (ProblemCnt.Value % 2 == 1)
+                ProblemHistory1.Add(problemResult);
+            else
+                ProblemHistory2.Add(problemResult);
+
+            if (ProblemCnt.Value == 100)
+            {
+                _sw.Stop();
+                if (!await ShowWindow())
+                    Environment.Exit(0);
+
+                NewGame();
+            }
 
             NewProblem();
+        }
+
+        private async Task<bool> ShowWindow()
+        {
+            var t = new TaskCompletionSource<bool>();
+            var currentWindow = new ResultDialog(this, t);
+            currentWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            currentWindow.Owner = Application.Current.MainWindow;
+            currentWindow.Show();
+            currentWindow.Focus();
+            var b = await t.Task;
+            currentWindow.Close();
+            return b;
         }
     }
 }
